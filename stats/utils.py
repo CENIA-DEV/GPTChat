27deece28e15d425201bac219e79ac58add90268
sdonoso <sebastian.a.donoso.b@gmail.com>
2024-09-26 23:55:10 -0300

    count votes from bucket

--- stats/utils.py
+++ stats/utils.py
@@ -1,6 +1,8 @@
 import glob
 import json
 import pandas as pd
+from google.cloud.storage import Client, transfer_manager
+
 # Definir el conteo de países y demás variables como en tu código
 country_count = {
     'Argentina': 0, 'Bolivia': 0, 'Brazil': 0, 'Chile': 0, 'Colombia': 0, 
@@ -51,3 +53,33 @@ def count_country_votes():
             
         }
     )
+
+def download_bucket_with_transfer_manager(
+    bucket_name, destination_directory="", workers=8, max_results=1000
+):
+    """Download all of the blobs in a bucket, concurrently in a process pool.
+
+    The filename of each blob once downloaded is derived from the blob name and
+    the `destination_directory `parameter. For complete control of the filename
+    of each blob, use transfer_manager.download_many() instead.
+
+    Directories will be created automatically as needed, for instance to
+    accommodate blob names that include slashes.
+    """
+    storage_client = Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    blob_names = [blob.name for blob in bucket.list_blobs(prefix="data_chat",max_results=max_results)]
+
+    results = transfer_manager.download_many_to_path(
+        bucket, blob_names, destination_directory=destination_directory, max_workers=workers
+    )
+
+    for name, result in zip(blob_names, results):
+        # The results list is either `None` or an exception for each blob in
+        # the input list, in order.
+
+        if isinstance(result, Exception):
+            print("Failed to download {} due to exception: {}".format(name, result))
+        else:
+            print("Downloaded {} to {}.".format(name, destination_directory + name))
