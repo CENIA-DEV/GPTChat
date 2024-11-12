684af52b3c05a89f6eddf67bd2e31338d6fde25b
sdonoso <sebastiandonoso@fastchat-web-gui.us-central1-a.c.teak-truck-377020.internal>
2024-11-12 19:51:28 +0000

    fix votes count

--- remote_logger_server.py
+++ remote_logger_server.py
@@ -1,43 +1,42 @@
+import logging
+import json
+import dotenv
 from flask import Flask, request, jsonify
 from google.cloud import storage
-import json
 from datetime import datetime
-import dotenv
 
 dotenv.load_dotenv()
 app = Flask(__name__)
 
-# Initialize Google Cloud Storage client
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 storage_client = storage.Client()
 
-# Define your bucket name
 BUCKET_NAME = "gpt-las-chat"
 
 
 @app.route("/upload-json", methods=["POST"])
 async def upload_json():
     try:
-        # Get the JSON data from the request
         data = request.get_json()
 
         if not data:
             return jsonify({"error": "No JSON data received"}), 400
 
-        # Create a unique filename using the current timestamp
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
         filename = f"uploaded_data_{timestamp}.json"
 
-        # Convert the JSON data to a string
         json_data = json.dumps(data, ensure_ascii=False)
 
-        # Get the bucket
         bucket = storage_client.bucket(BUCKET_NAME)
 
-        # Create a new blob and upload the JSON data
         blob = bucket.blob("data_chat/" + filename)
-        await blob.upload_from_string(json_data, content_type="application/json")
+        blob.upload_from_string(json_data, content_type="application/json")
+        logger.info(f"message: File {filename} uploaded successfully, status: 200")
 
         return jsonify({"message": f"File {filename} uploaded successfully"}), 200
     except Exception as e:
+        logger.info(f"error: {str(e)}, status:500")
         return jsonify({"error": str(e)}), 500
-
